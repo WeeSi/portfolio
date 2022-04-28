3d847f1c6ad5556f25366f509968136144ef6b78
Franck <francklucasehui@gmail.com>
2022-04-28 18:26:08 +0200

    last prod

---
+++ src/components/sidebar/Navigation.jsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { motion } from "framer-motion";
+import { MenuItem } from "./MenuItem";
+import { HashLink } from "react-router-hash-link";
+
+const items = [
+  {
+    name: "A propos",
+    url: "/#about",
+  },
+  {
+    name: "Compétence",
+    url: "/#skills",
+  },
+  {
+    name: "Expériences",
+    url: "/#exp",
+  },
+  {
+    name: "Projets",
+    url: "/#projects",
+  },
+];
+
+const variants = {
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
+};
+
+export const Navigation = ({ isOpen, closeSideBar }) => (
+  <motion.ul
+    style={{ pointerEvents: isOpen ? "" : "none" }}
+    className="nav-ul"
+    variants={variants}
+  >
+    {items.map((i) => (
+      <HashLink onClick={() => closeSideBar()} to={`${i.url}`}>
+        <MenuItem name={i.name} i={i} key={i.name} />
+      </HashLink>
+    ))}
+  </motion.ul>
+);
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,137 @@
+import React, { useRef, useEffect } from "react";
+import { motion, useCycle } from "framer-motion";
+import { MenuToggle } from "./MenuToggle";
+import { Navigation } from "./Navigation";
+import { useDimensions } from "../../hooks/useDimensions";
+import Switch from '@mui/material/Switch';
+import { styled } from '@mui/material/styles';
+
+
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+    width: 62,
+    height: 34,
+    padding: 7,
+    '& .MuiSwitch-switchBase': {
+      margin: 1,
+      padding: 0,
+      transform: 'translateX(6px)',
+      '&.Mui-checked': {
+        color: '#fff',
+        transform: 'translateX(22px)',
+        '& .MuiSwitch-thumb:before': {
+          backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+            '#fff',
+          )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+        },
+        '& + .MuiSwitch-track': {
+          opacity: 1,
+          backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+        },
+      },
+    },
+    '& .MuiSwitch-thumb': {
+      backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+      width: 32,
+      height: 32,
+      '&:before': {
+        content: "''",
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        left: 0,
+        top: 0,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          '#fff',
+        )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+      },
+    },
+    '& .MuiSwitch-track': {
+      opacity: 1,
+      backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+      borderRadius: 20 / 2,
+    },
+  }));
+
+const data = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: "circle(30px at 40px 40px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
+const Sidebar = (props) => {
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  const containerRef = useRef(null);
+  const node = useRef(null);
+  const { height } = useDimensions(containerRef);
+
+  useEffect(() => {
+    // add when mounted
+    document.addEventListener("mousedown", handleClick);
+    // return function to be called when unmounted
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleClick = (e) => {
+    if (e.target.classList.contains("modal-backdrop")) {
+      toggleOpen(false);
+    }
+  };
+
+  return (
+    <div>
+      <motion.nav
+        initial={false}
+        animate={isOpen ? "open" : "closed"}
+        custom={height}
+        ref={containerRef}
+        style={{ height: isOpen ? "" : "fit-content" }}
+      >
+        <>
+          <motion.div
+            variants={data}
+            className="fixed inset-0 transition-opacity"
+          >
+            {" "}
+            <div className="modal-backdrop absolute inset-0 opacity-80 cursor-pointer"></div>
+          </motion.div>
+          <motion.div
+            ref={node}
+            className="shadow-lg background-nav"
+            variants={data}
+          />
+          <motion.div className="absolute bottom-5 left-5" variants={data}>
+            <MaterialUISwitch onClick={() => props.setDark()} checked={props.dark} defaultChecked />
+          </motion.div>
+          <Navigation
+            closeSideBar={() => {
+              toggleOpen(false);
+            }}
+            isOpen={isOpen}
+          />
+          <MenuToggle toggle={() => toggleOpen()} />
+        </>
+      </motion.nav>
+    </div>
+  );
+};
+
+export default Sidebar;
